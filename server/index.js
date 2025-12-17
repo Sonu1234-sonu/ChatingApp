65ab71cc899d102cb95463d9e19d1b9876d21c56
Sonu1234-sonu <sonulovewanshi999@gmail.com>
2025-12-17 18:18:48 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "https://chatingapplication1.netlify.app",
+        origin: " http://localhost:5173",
         credentials: true
     }
 });
@@ -28,7 +28,7 @@ io.on("connection", (socket) => {
     // You can handle socket events here
 });
 
-app.use(cors({ origin: "https://chatingapplication1.netlify.app", credentials: true }));
+app.use(cors({ origin: " http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
